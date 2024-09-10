1cf2de22f2b72e3e04252d7635ad39a3a12af942
r.potapov <r.potapov@favbet.tech>
2024-09-10 10:50:42 +0200

    small changes

--- src/app/api/courses/[courseId]/route.ts
+++ src/app/api/courses/[courseId]/route.ts
@@ -6,7 +6,7 @@ export async function GET(req: Request, { params }: { params: { courseId: string
   const geCourse = mainCourses.find(({ hash }) => hash === params.courseId);
 
   if (!geCourse) {
-    return NextResponse.json({ message: 'News not found' }, { status: 404 });
+    return NextResponse.json({ message: 'Course not found' }, { status: 404 });
   }
 
   return NextResponse.json(geCourse);
